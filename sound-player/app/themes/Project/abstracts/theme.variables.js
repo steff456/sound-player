d4dd90baf0e4d88fba0b0381a668735c1418be9b
steff456 <steff456@hotmail.com>
2018-10-27 19:35:37 -0500

    init

---
+++ sound-player/app/themes/Project/abstracts/theme.variables.js
@@ -0,0 +1,12 @@
+// Theme variables for Inline Styles or Styled Components Styles
+
+const theme = {
+  primaryColor: '#282828',
+  secondaryColor: '#04ba4f',
+  textColor: '#a9a9a9',
+  darkGrey: '#404040',
+  midGrey: '#a9a9a9',
+  lightGrey: '#ffffff',
+};
+
+export default theme;
