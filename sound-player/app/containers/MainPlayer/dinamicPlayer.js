ddb482d1a6631bbe05df452dddab9632556dd545
steff456 <steff456@hotmail.com>
2018-10-29 14:02:18 -0500

    layout finished

--- sound-player/app/containers/MainPlayer/dinamicPlayer.js
+++ sound-player/app/containers/MainPlayer/dinamicPlayer.js
@@ -8,12 +8,16 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Time = styled.div`
-  height: 12px;
-  margin-bottom: 3px;
+  height: 8px;
+  width: 100%;
   overflow: hidden;
+  display: flex;
+  justify-content: end;
+  align-items: end;
+  background-color: ${props => props.theme.midGrey};
 `;
 
-const Container = styled.div`
+const DinamicContainer = styled.div`
   height: 90px;
   position: absolute;
   bottom: 100px;
@@ -28,13 +32,65 @@ const Container = styled.div`
   right: 0;
 `;
 
+const SongName = styled.div`
+  color: ${props => props.theme.lightGrey};
+  height: 100%;
+  width: 100%;
+  position: relative;
+  font-size: 17px;
+  font-weight: bold;
+`;
+
+const AlbumName = styled.div`
+  color: ${props => props.theme.textColor};
+  height: 100%;
+  width: 100%;
+  font-size: 14px;
+`;
+
+const InfoSong = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: start;
+  padding: 10px;
+  margin-left: 200px;
+  flex-direction: column;
+`;
+
+const NumberTrack = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  width: 100%;
+  font-size: 12px;
+  margin-bottom: 9px;
+`;
+
+const LineTrack = styled.div`
+  display: flex;
+  width: 60%;
+  background-color: ${props => props.theme.secondaryColor};
+  height: 8px;
+`;
+
 /* eslint-disable react/prefer-stateless-function */
 export class DinamicPlayer extends React.Component {
   render() {
     return (
-      <Container>
-        <Time />
-      </Container>
+      <DinamicContainer>
+        <InfoSong>
+          <SongName>Holi</SongName>
+          <AlbumName>Nombre del Albumcito</AlbumName>
+          <NumberTrack>
+            <div>0:00</div>
+            <div>5:00</div>
+          </NumberTrack>
+          <Time>
+            <LineTrack />
+          </Time>
+        </InfoSong>
+      </DinamicContainer>
     );
   }
 }
